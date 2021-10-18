871064754cd1bad7548414b4d67fc0a9e6c042c1
chano <chano22@naver.com>
2021-10-18 18:31:45 +0900

    first-init

---
+++ src/main/java/com/sample/circuit/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.sample.circuit.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
